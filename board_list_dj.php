f1d30dc7ea9913e605392e789b085b8d7e732d45
yeji <ellen10735@gmail.com>
2021-06-12 14:35:19 +0900

    board_list_dj

---
+++ board_list_dj.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+<head> 
+<meta charset="utf-8">
+<title>PHP 프로그래밍 입문</title>
+<link rel="stylesheet" type="text/css" href="./css/common.css?after">
+<link rel="stylesheet" type="text/css" href="./css/board.css">
+</head>
+<body> 
+<header>
+    <?php include "header.php";?>
+</header>  
+<section>
+	<div id="main_img_bar">
+        <img src="./img/mymain_img.png">
+    </div>
+   	<div id="board_box">
+	    <h3>
+	    	두정역 > 목록보기
+		</h3>
+	    <ul id="board_list">
+				<li>
+					<span class="col1">번호</span>
+					<span class="col2">목적지</span>
+					<span class="col3">탑승시간</span>
+					<span class="col4">작성자</span>
+					<span class="col5">등록일</span>
+				</li>
+<?php
+    $mode="dj";
+	if (isset($_GET["page"]))
+		$page = $_GET["page"];
+	else
+		$page = 1;
+
+	$con = mysqli_connect("localhost", "root", "12345", "taxi");
+	$sql = "select * from board_dj order by num desc";
+	$result = mysqli_query($con, $sql);
+	$total_record = mysqli_num_rows($result); // 전체 글 수
+
+	$scale = 3;
+
+	// 전체 페이지 수($total_page) 계산 
+	$total_page = ceil($total_record/$scale); 
+ 
+	// 표시할 페이지($page)에 따라 $start 계산  
+	$start = ($page - 1) * $scale;      
+
+	$number = $total_record - $start;
+
+   for ($i=$start; $i<$start+$scale && $i < $total_record; $i++)
+   {
+      mysqli_data_seek($result, $i);
+      // 가져올 레코드로 위치(포인터) 이동
+       
+      $row = mysqli_fetch_array($result);
+      // 하나의 레코드 가져오기
+	  $num         = $row["num"];
+      $destination = $row["destination"];
+      $time        = $row["time"];   
+	  $point       = $row["point"];      //포인트를 board에 저장하도록 수정하기!
+	  $name        = $row["name"];
+      $regist_day  = $row["regist_day"];
+       
+?>
+				<li>
+					<span class="col1"><?=$number?></span>
+					<span class="col2"><a href="board_view.php?num=<?=$num?>&page=<?=$page?>&mode=<?=$mode?>"><?=$destination?></a></span>
+					<span class="col3"><?=$time?></span>
+					<span class="col4"><?=$name."(".$point.")"?></span>
+					<span class="col5"><?=$regist_day?></span>
+				</li>	
+<?php
+   	   $number--;
+   }
+   mysqli_close($con);
+
+?>
+	    	</ul>
+			<ul id="page_num"> 	
+<?php
+	if ($total_page>=2 && $page >= 2)	
+	{
+		$new_page = $page-1;
+		echo "<li><a href='board_list_dj.php?page=$new_page'>◀ 이전</a> </li>";
+	}		
+	else 
+		echo "<li>&nbsp;</li>";
+
+   	// 게시판 목록 하단에 페이지 링크 번호 출력
+   	for ($i=1; $i<=$total_page; $i++)
+   	{
+		if ($page == $i)     // 현재 페이지 번호 링크 안함
+		{
+			echo "<li><b> $i </b></li>";
+		}
+		else
+		{
+			echo "<li><a href='board_list_dj.php?page=$i'> $i </a><li>";
+		}
+   	}
+   	if ($total_page>=2 && $page != $total_page)		
+   	{
+		$new_page = $page+1;	
+		echo "<li> <a href='board_list_dj.php?page=$new_page'>다음 ▶</a> </li>";
+	}
+	else 
+		echo "<li>&nbsp;</li>";
+?>
+			</ul> <!-- page -->	    	
+			<ul class="buttons">
+				<li>
+<?php 
+    if($userid) {            
+?>
+                    <a href="board_form.php?mode=<?=$mode?>"><button>글쓰기</button></a> <!--mode="ca"-->
+<?php
+	} else {
+?>
+					<a href="javascript:alert('로그인 후 이용해 주세요!')"><button>글쓰기</button></a>
+<?php
+	}
+?>
+				</li>
+			</ul>
+	</div> <!-- board_box -->
+</section> 
+<footer>
+    <?php include "footer.php";?>
+</footer>
+</body>
+</html>
