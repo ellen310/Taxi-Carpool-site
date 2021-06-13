f11ec491664027e13297caebeeadcd197015d1e9
yeji <ellen10735@gmail.com>
2021-06-14 00:17:22 +0900

    driver_main

---
+++ driver_main.php
@@ -0,0 +1,11 @@
+     <div id="main_img_bar">
+            <img src="./img/mymain_img.png">
+        </div>
+        <div id="main_content">
+            
+            <section>
+	       <?php include "board_list_dri.php";?>        
+            </section> 
+              
+            
+        </div>
