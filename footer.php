83b41f184cb697fe87b15a52da9936d49e152d2d
yeji <ellen10735@gmail.com>
2021-06-15 00:08:11 +0900

    footer

---
+++ footer.php
@@ -0,0 +1,7 @@
+        <div id="footer_content">
+            <p id="footer_logo">택시 동승 사이트 | <span>허예지</span></p>
+            <ul id="download">
+                <li>이메일 주소</li>
+                <li>-ellen10735@gmail.com</li>
+            </ul>
+        </div>
