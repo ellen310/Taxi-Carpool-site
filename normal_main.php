8897dd1cf2a47d96f6f7aa31ca4b135215b5a735
yeji <ellen10735@gmail.com>
2021-07-01 23:27:14 +0900

    normal

---
+++ normal_main.php
@@ -0,0 +1,26 @@
+     <div id="main_img_bar">
+            <img src="./img/mymain_img.png">
+        </div>
+        <div id="main_content">
+        <div>
+            
+            
+            <table>
+                <tr>
+                    <th><h3>학교 - 천안역</h3></th>
+                    <th><h3>학교 - 두정역</h3></th>
+                    <th><h3>학교 - 터미널</h3></th>
+                    <th><h3>&nbsp;&nbsp;&nbsp;&nbsp;기타</h3></th>
+                </tr>
+                <tr>
+                    <th><a href="board_list_ca.php"><img src="img/ca.jpg" width="200px" height="200px" alt="사진없음" style="border: 1px solid; margin-left: 40px;"></a></th>
+                    <th><a href="board_list_dj.php"><img src="img/dj.jpg" width="200px" height="200px" alt="사진없음" style="border: 1px solid; margin-left: 40px;"></a></th>
+                    <th><a href="board_list_ter.php"><img src="img/ter.jpg" width="200px" height="200px" alt="사진없음" style="border: 1px solid; margin-left: 40px;"></a></th>
+                    <th><a href="board_list_etc.php"><img src="img/etc.webp" width="200px" height="200px" alt="사진없음" style="border: 1px solid; margin-left: 40px;"></a></th>
+                </tr>
+            </table>
+                
+              
+            </div>
+            
+        </div>
